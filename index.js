d51e8cb47da1cde5f6e759937841c83397bd3fce
PizzerSpring <vladi-slav96@mail.ru>
2025-11-09 17:01:11 +0700

    Import image and style

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(12597);
+
+import LogoImage from './assets/images/Logo.svg';
+import './assets/styles/style.css';
+
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,47 @@
+
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: './index.js'
+    mode: 'development',
+    entry: path.resolve(__dirname, 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        clean: true,
+        assetModuleFilename: 'assets/images/[name][ext]'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'index.html') })
+    ],
+    module: {
+        rules: [
+            { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/, },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                 type: 'asset/resource'
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.css'],
+    },
+    devServer: {
+        port: 5000,
+        open: true
+    }
+    /* module: {
+         rules: [
+             { test: /\.svg$/, use: 'svg-inline-loader' },
+             { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+             { test: /\.(js)$/, use: 'babel-loader' }
+         ]
+     },
+     devServer: {
+         static: {
+             directory: path.join(__dirname, '/'), 
+         },
+     },*/
 }
